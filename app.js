87cd140e043fef22fd21d124f54864063a41ae48
Lennie Nurse <lnursej@gmail.com>
2023-09-10 13:35:52 -0400

    added db

--- app.js
+++ app.js
@@ -3,32 +3,40 @@
 
 // Import external stuff (libraries)
 // Import express library
-const express = require('express');
-const cors = require('cors');
+const express = require("express")
+const cors = require("cors")
+const db = require("./db")
 
 // Import OUR stuff (our files, our components)
-const studentsController = require('./controllers/studentsController');
-const studentsControllerV2 = require('./controllers/v2/studentsControllerV2');
+const studentsController = require("./controllers/studentsController")
+const studentsControllerV2 = require("./controllers/v2/studentsControllerV2")
 
 // Init express application
-const app = express();
+const app = express()
 
 // Set up middleware
 // Functions that will work with req, res before
 // the final route handler function
-app.use(cors());
+app.use(cors())
 
 // Controllers
-app.use('/students', studentsController);
-app.use('/v2/students', studentsControllerV2);
+app.use("/students", studentsController)
+app.use("/v2/students", studentsControllerV2)
 
 // Define our routes
+app.get("/tests", async (request, response) => {
+  try {
+    const tests = await db.any("SELECT * FROM tests;")
+
+    response.status(200).json({ data: tests })
+  } catch (err) {
+    response.status(500).json({ error: err.message })
+  }
+})
 
 // Healthcheck route
-app.get('/', (request, response) => {
-  response.status(200).json({ data: 'Service is running' });
-});
+app.get("/", (request, response) => {
+  response.status(200).json({ data: "Service is running" })
+})
 
-
-
-module.exports = app;
+module.exports = app
